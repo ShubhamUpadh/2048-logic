9e0791bf377b311e3b8a6c8980d2633083f393df
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-04-03 04:45:24 +0530

    Completed work on move_right and move_left function

--- move.py
+++ move.py
@@ -34,7 +34,6 @@ def move_down(grid):
                 grid[i][j] = 0
             else:
                 grid[i][j] = q.popleft()
-    #return grid
     for i in range(3,0,-1):      #addition loop
         for j in range(4):
             if grid[i][j] == grid[i-1][j]:
@@ -45,6 +44,52 @@ def move_down(grid):
                 grid[i-1][j] = 0
     return grid
 
+def move_left(grid):
+    for i in range(4):  # remove extra 0s
+        q = deque()
+        for j in range(4):
+            if grid[i][j] != 0:
+                q.append(grid[i][j])
+                grid[i][j] = 0
+        for j in range(4):
+            if len(q) != 0:
+                grid[i][j] = q.popleft()
+            else:
+                grid[i][j] = 0
+    #printGrid(grid)
+    for i in range(4):
+        for j in range(3):
+            if grid[i][j] == grid[i][j+1]:
+                grid[i][j] = 2*grid[i][j]
+                grid[i][j+1] = 0
+            elif grid[i][j] == 0 and grid[i][j+1] != 0:
+                grid[i][j] = grid[i][j+1]
+                grid[i][j+1] = 0
+    return grid
+
+def move_right(grid):
+    for i in range(4):  # remove extra 0s
+        q = deque()
+        for j in range(3,-1,-1):
+            if grid[i][j] != 0:
+                q.append(grid[i][j])
+                grid[i][j] = 0
+        for j in range(3,-1,-1):
+            if len(q) != 0:
+                grid[i][j] = q.popleft()
+            else:
+                grid[i][j] = 0
+    printGrid(grid)
+    for i in range(4):
+        for j in range(3,0,-1):
+            if grid[i][j] == grid[i][j-1]:
+                grid[i][j] = 2*grid[i][j]
+                grid[i][j-1] = 0
+            elif grid[i][j] == 0 and grid[i][j-1] != 0:
+                grid[i][j] = grid[i][j-1]
+                grid[i][j-1] = 0
+    return grid
+
 def printGrid(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -52,17 +97,17 @@ def printGrid(grid):
             if j != len(grid[0]) - 1:
                 print(end=" | ")
         print()
+    print("")
 
 def defineGrid():
     grid = [[4,2,8,4],
             [2,0,2,2],
             [0,0,0,2],
-            [0,0,0,0]]
+            [0,0,2,0]]
     return grid
 
 if __name__ == "__main__":
     grid = defineGrid()
     printGrid(grid)
-    print()
-    grid = move_down(grid)
+    grid = move_right(grid)
     printGrid(grid)
