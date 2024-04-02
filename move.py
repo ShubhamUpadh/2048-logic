a9d302d7476a955b98a3edbb5357b300d94f41d8
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-04-03 04:14:58 +0530

    Completed work on move_down function

--- move.py
+++ move.py
@@ -21,6 +21,30 @@ def move_up(grid):
                 grid[i][j] = q.popleft()
     return grid
 
+def move_down(grid):
+    
+    for j in range(4):          # remove extra 0 spaces using queues
+        q = deque()
+        for i in range(3,-1,-1):
+            if grid[i][j] != 0:
+                q.append(grid[i][j])
+                grid[i][j] = 0
+        for i in range(3,-1,-1):
+            if len(q) == 0:
+                grid[i][j] = 0
+            else:
+                grid[i][j] = q.popleft()
+    #return grid
+    for i in range(3,0,-1):      #addition loop
+        for j in range(4):
+            if grid[i][j] == grid[i-1][j]:
+                grid[i][j] = 2 * grid[i][j]
+                grid[i-1][j] = 0
+            elif grid[i][j] == 0 and grid[i-1][j] != 0:
+                grid[i][j] = grid[i-1][j]
+                grid[i-1][j] = 0
+    return grid
+
 def printGrid(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -30,15 +54,15 @@ def printGrid(grid):
         print()
 
 def defineGrid():
-    grid = [[0,0,4,0],
-            [0,0,0,0],
-            [0,0,0,4],
-            [2,0,2,2]]
+    grid = [[4,2,8,4],
+            [2,0,2,2],
+            [0,0,0,2],
+            [0,0,0,0]]
     return grid
 
 if __name__ == "__main__":
     grid = defineGrid()
     printGrid(grid)
     print()
-    grid = move_up(grid)
+    grid = move_down(grid)
     printGrid(grid)
